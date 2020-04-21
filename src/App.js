f152c8824c2a6a538bef12710276e0461e91b510
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2020-04-22 00:46:20 +0900

    create class and functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onClick={() => {
-          console.log('I am clicked.');
-        }}
-      />
-    </React.Fragment>
-  );
+// クラスコンポーネント
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input
+          type="text"
+          onClick={() => {
+            console.log('I am clicked.');
+          }}
+        />
+      </React.Fragment>
+    );
+  }
 }
+
+// // ファンクショナルコンポーネント
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//     </div>
+//   );
+// };
+
+// const Cat = () => {
+//   return <div>Meow!</div>;
+// };
+
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onClick={() => {
+//           console.log('I am clicked.');
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
 
 export default App;
